a1248ea6de16a0baa67c1fc47be295e979e75ea1
Tamás Mitro <tamas.mitro@ericsson.com>
2019-07-17 11:49:58 +0200

    ADD: commit3

---
+++ src/hu/mitro/decorator/compozit/Directory.java
@@ -0,0 +1,36 @@
+package hu.mitro.decorator.compozit;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class Directory extends Item {
+
+	Set<Item> subDirectories;
+
+	public Directory(String name) {
+		super(name);
+		subDirectories = new HashSet<>();
+	}
+
+	public void add(Item item) {
+		subDirectories.add(item);
+	}
+
+	public void remove(Item item) {
+		subDirectories.remove(item);
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder stringBuilder = new StringBuilder("---- " + name + "\n");
+		for (Item item : subDirectories) {
+			if (item instanceof Directory) {
+				stringBuilder.append("\t" + item.toString() + "\n");
+			} else {
+				stringBuilder.append("\t\t" + item.toString() + "\n");
+			}
+		}
+		return stringBuilder.toString();
+	}
+}
---
+++ src/hu/mitro/decorator/compozit/Item.java
@@ -0,0 +1,10 @@
+package hu.mitro.decorator.compozit;
+
+public abstract class Item {
+
+	String name;
+
+	public Item(String name) {
+		this.name = name;
+	}
+}
